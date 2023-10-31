168f41c3e45bd3eeea77779ce95cf43e354d89fe
tarik <123542696+tarikkirgin@users.noreply.github.com>
2023-10-31 15:01:44 +0000

    add file read functionality

--- StepsTask1.c
+++ StepsTask1.c
@@ -43,6 +43,59 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 int main() {
+    char* filename = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+    // exit if file is null
+    if (file == NULL) {
+        perror("File I/O");
+        return 1;
+    }
 
+    // declare array of structs
+    FITNESS_DATA data[300];
 
+    // create buffer
+    int buffer_size = 50;
+    char line_buffer[buffer_size];
+
+    // initalise variable to store end of array
+    int tail = 0;
+
+    // read in file contents
+    while (fgets(line_buffer, buffer_size, file) != NULL)
+    {
+        // create variables to hold data
+        char date[11];
+        char time[6];
+        char steps[10];
+
+        // extract data from line of file
+        tokeniseRecord(line_buffer, ",", date, time, steps);
+
+        // create new FITNESS_DATA struct
+        FITNESS_DATA row;
+
+        // populate struct with values extracted from file
+        strcpy(row.date, date);
+        strcpy(row.time, time);
+        row.steps = atoi(steps); // use atoi() to convert the steps string to an integer
+
+        // add struct to array
+        data[tail] = row;
+        // increment end of array marker
+        tail++;
+    }
+    // close file
+    fclose(file);
+
+    // print number of records
+    printf("Number of records in file: %d\n", tail);
+
+    // print out first 3 rows of data
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%s/%s/%d\n", data[i].date, data[i].time, data[i].steps);
+    }
+
+    return 0;
 }
