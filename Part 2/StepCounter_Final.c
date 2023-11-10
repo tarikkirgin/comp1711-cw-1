d220169d79156825dfd1ec42133fb3387ce4c7a9
tarik <123542696+tarikkirgin@users.noreply.github.com>
2023-11-10 16:56:08 +0000

    format file

--- Part 2/StepCounter_Final.c
+++ Part 2/StepCounter_Final.c
@@ -13,32 +13,33 @@
 // Inputs: character array representing a row; the delimiter character
 // Ouputs: date character array; time character array; steps character array
 void tokeniseRecord(const char *input, const char *delimiter,
-                    char *date, char *time, char *steps) {
+                    char *date, char *time, char *steps)
+{
     // Create a copy of the input string as strtok modifies the string
     char *inputCopy = strdup(input);
     
     // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
-    if (token != NULL) {        strcpy(date, token);
+    if (token != NULL)
+    {
+        strcpy(date, token);
     }
     
     token = strtok(NULL, delimiter);
-    if (token != NULL) {
+    if (token != NULL)
+    {
         strcpy(time, token);
     }
     
     token = strtok(NULL, delimiter);
-    if (token != NULL) {
+    if (token != NULL)
+    {
         strcpy(steps, token);
     }
     
     // Free the duplicated string
     free(inputCopy);
-
-                    }
-
-
-
+}
 
 // Complete the main function
 int main() {
